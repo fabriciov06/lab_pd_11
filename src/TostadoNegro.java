d4e49084d55f038e44d6b2eac93d33b5a3be9f8c
Fabricio Vera <sebastianvt2006@gmail.com>
2026-06-20 17:25:00 -0500

    feat: Cree la clase 'TostadoNegro' con su constructor y el retorno de su precio

---
+++ src/TostadoNegro.java
@@ -0,0 +1,10 @@
+public class TostadoNegro extends Cafe{
+
+    public TostadoNegro(){
+        descripcion = "Cafe Tostado Negro";
+    }
+
+    public double costo(){
+        return 0.99;
+    }
+}
