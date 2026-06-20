f7424986ba8d7f6b5c8beaf75fad4685e4154892
Fabricio Vera <sebastianvt2006@gmail.com>
2026-06-20 17:59:29 -0500

    feat: Termine

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,34 @@ public static void main(String[] args) {
     bebida = new Soya(bebida);
     bebida = new Crema(bebida);
     bebida = new Moca(bebida);
-
     System.out.println(bebida.getDescripcion() + "\nTotal: $" + bebida.costo());
 
 
+    Cafe bebida2 = new Descafeinado();
+    bebida2 = new Moca(bebida2);
+    bebida2 = new Moca(bebida2);
+    bebida2 = new Soya(bebida2);
+    bebida2 = new Leche(bebida2);
+    System.out.println(bebida2.getDescripcion() + "\nTotal: $" + bebida2.costo());
 
 
+    Cafe bebida3 = new Batido();
+    bebida3 = new Crema(bebida3);
+    bebida3 = new Crema(bebida3);
+    bebida3 = new Leche(bebida3);
+    bebida3 = new Leche(bebida3);
+    bebida3 = new Soya(bebida3);
+    bebida3 = new Soya(bebida3);
+    bebida3 = new Leche(bebida3);
+    bebida3 = new Leche(bebida3);
+    System.out.println(bebida3.getDescripcion() + "\nTotal: $" + bebida3.costo());
+
+
+    Cafe bebida4 = new TostadoNegro();
+    bebida4 = new Crema(bebida4);
+    bebida4 = new Crema(bebida4);
+    bebida4 = new Soya(bebida4);
+    bebida4 = new Soya(bebida4);
+    bebida4 = new Soya(bebida4);
+    System.out.println(bebida4.getDescripcion() + "\nTotal: $" + bebida4.costo());
 }
